bfd59d08c683a7d9037fecf9eda661a37df09bf4
Bereza <>
2014-12-24 06:08:41 -0300

    Bereza

--- src/ordericecream/IceCream.java
+++ src/ordericecream/IceCream.java
@@ -1,13 +1,17 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package ordericecream;
-
-abstract class IceCream {
-
+abstract class IceCream {  
+ /*3.	Фабрика производит мороженное "Чудо-буренка" для продажи на территории
+    РБ, и мороженное "Белорусские узоры" для продажи на территории РФ.
+    В проекте присутствует класс, который генерирует количество заказов.
+    
+    
+7.	Программа создает сюжетных персонажей, с которыми сражается герой,
+    в зависимости от уровня сложности. Три уровня сложности: легкий, средний,
+    сложный. Виды персонажей: монстры, зомби, солдаты. В зависимости от уровня
+    сложности персонажи имеют разные сопротивляемость, навыки и т.д. В проекте
+    присутствует класс, который генерирует, какое число персонажей каждого
+    вида будет создано. 
+*/
     String name;
     String country;
     int number;
@@ -26,12 +30,10 @@ abstract class IceCream {
         System.out.println("Preparing " + name);
         System.out.println("Blending the mixture...");
         System.out.println("Cooling...");
-        System.out.println("Packaging and bundling the finished product...");
-        
+        System.out.println("Packaging and bundling the finished product...");   
     }
 }
 class MiracleBurenka extends IceCream {
-
     public MiracleBurenka() {
         name = "Miracle Burenka";  
         country = "Belarus";
@@ -39,13 +41,11 @@ class MiracleBurenka extends IceCream {
     }
 }
 class BelarusianDesigns extends IceCream {
-
     public BelarusianDesigns() {
         name = "Belarusian designs";
         country = "Russia";
         number= (int) (Math.random()*100 + 10);
     }
-
 }
 class IceCreamStore {
     public IceCream orderIceCream (String type) {
